7bbf7a0a49c62add2371d90fd401220182c3e061
qiuhaotc <qiu66hao@163.com>
2022-04-17 13:06:31 +0800

    partial finished

---
+++ src/ConcatenateVideos/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConcatVideos;
+
+CancellationTokenSource cancellationTokenSource;
+bool finishedRunning;
+
+cancellationTokenSource = new CancellationTokenSource();
+
+try
+{
+    var fileNames = Environment.GetCommandLineArgs().Skip(1);
+    Console.WriteLine("Start concatenate videos for: " + string.Join(Environment.NewLine, fileNames));
+    await ConcatenateUtils.Run(fileNames, cancellationTokenSource.Token);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+finishedRunning = true;
+Console.WriteLine("Running finished, press any key to exit.");
+Console.ReadLine();
+
+// See https://aka.ms/new-console-template for more information
+AppDomain.CurrentDomain.ProcessExit += new EventHandler((u, v) =>
+{
+    cancellationTokenSource.Cancel();
+
+    while (!finishedRunning)
+    {
+        Console.WriteLine("Wait ffmpeg exiting...");
+        Thread.Sleep(100);
+    }
+
+    cancellationTokenSource.Dispose();
+});
---
+++ src/ConvertCommon/ConvertConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace ConvertCommon
+{
+    public class ConvertConfiguration
+    {
+        public string AudioType { get; set; } = string.Empty;
+        public bool OverrideExistFile { get; set; }
+        public int ThreadsCount { get; set; }
+        public string FFmpegExecutablePath { get; set; } = string.Empty;
+        public string PrefixForConcatenate { get; set; } = string.Empty;
+    }
+}
